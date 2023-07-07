d80a46592cc1c9d5954b1e76e9c041fa52d5c824
Joshua Harding <josh@statewidesoftware.com>
2023-07-07 03:21:53 -0400

    looking great

--- lib/tokenbucket_server.rb
+++ lib/tokenbucket_server.rb
@@ -23,19 +23,34 @@ class Server
     start_gc_thread
     @logger = ::Logger.new(STDOUT)
     @logger.level= ::Logger::DEBUG
+    @clients = []
   end
 
   def start
     @logger.info { "Starting server on port #{@port}" }
     loop do
       Thread.start(@server.accept) do |client|
-        @logger.debug { "Accepted connection from #{client.pretty_print}" }
-        handle_client(client)
+          @logger.info { "Accepted connection from #{client.pretty_print}" }
+          @clients << client
+          handle_client(client)
+        ensure
+          @clients.delete(client)
       end
     end
   end
 
   private
+
+  def bucket_stats(bucket_name)
+    bucket_info = @buckets[bucket_name]
+    bucket_info[:mutex].synchronize do
+      "tokens=#{bucket_info[:bucket].tokens},rate=#{bucket_info[:bucket].rate},capacity=#{bucket_info[:bucket].capacity}"
+    end
+  end
+
+  def valid_bucket_name?(bucket_name)
+    bucket_name&.match?(/\A[\p{L}\p{N}\p{Pc}\p{M}\p{S}]+\z/) == true
+  end
 
   def handle_client(client)
     while (line = client.gets)
@@ -43,28 +58,52 @@ class Server
       return unless line && line.instance_of?(String)
 
       command, bucket_name, parameter = line.split
-      bucket_info = @buckets[bucket_name]
+      bucket_info = @buckets[bucket_name] if valid_bucket_name?(bucket_name)
       case command&.upcase
       when "CONSUME"
+        # require to have a bucket name
+        if bucket_name.nil?
+          client.puts "ERROR CONSUME requires a bucket name"
+          next
+        end
+        # also make sure it's a valid bucket name. it has to be a unicode string
+        # with no spaces
+        unless valid_bucket_name?(bucket_name)
+          client.puts "ERROR Invalid bucket name"
+          next
+        end
         if bucket_info[:mutex].synchronize { bucket_info[:bucket].consume }
-          client.puts "OK"
+          # put back to the client the number of tokens left and some stats
+          client.puts "OK #{bucket_stats(bucket_name)}"
         else
           wait_time = bucket_info[:mutex].synchronize { bucket_info[:bucket].time_until_next_token }
-          client.puts "WAIT #{wait_time}"
+          client.puts "WAIT #{wait_time} #{bucket_stats(bucket_name)}"
         end
       when "RATE"
         new_rate = parameter.to_f
         bucket_info[:mutex].synchronize { bucket_info[:bucket].set_rate(new_rate) }
-        client.puts "Rate set to #{new_rate}"
+        client.puts "OK RATE set to #{new_rate} for bucket #{bucket_name}"
       when "CAPACITY"
         new_capacity = parameter.to_i
         bucket_info[:mutex].synchronize { bucket_info[:bucket].set_capacity(new_capacity) }
-        client.puts "Capacity set to #{new_capacity}"
+        client.puts "OK CAPACITY set to #{new_capacity} for bucket #{bucket_name}"
+      when "STATS"
+        # require to have a bucket name
+        if bucket_name.nil?
+          client.puts "ERROR STATS require a bucket name"
+          next
+        end
+        client.puts bucket_stats(bucket_name)
+      when "STATUS"
+        # cram all this on one line, OK clients=2 buckets=3, bucket_name=XXX,rate=yyy.
+        all_bucket_statuses = @buckets.map { |name, _| "#{name}=[#{bucket_stats(name)}]" }.join(",")
+        client.puts "OK STATUS clients=#{@clients.count} buckets=#{@buckets.count} #{all_bucket_statuses}"
       else
-        client.puts "Unknown command #{command}. Valid commands are CONSUME and RATE"
+        client.puts "ERROR Unknown command #{command}. Valid commands are CONSUME and RATE"
       end
     end
   ensure
+    @logger.info { "Closing connection to #{client.pretty_print}" }
     client.close
   end
 
@@ -75,6 +114,7 @@ class Server
         now = Time.now
         @buckets.each do |name, info|
           if now - info[:bucket].last_used > @gc_threshold
+            @logger.info { "Removing bucket #{name} from memory" }
             @buckets.delete(name)
           end
         end
